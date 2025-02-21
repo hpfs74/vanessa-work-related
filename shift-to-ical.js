5ae0c3d59ef7f130d6b5da72d9e7f73018807f16
Matteo Salvestrini <matteo.salvestrini@icloud.com>
2025-02-21 09:01:14 +0100

    wip
    Signed-off-by: Matteo Salvestrini <matteo.salvestrini@icloud.com>

--- shift-to-ical.js
+++ shift-to-ical.js
@@ -1,7 +1,20 @@
+#!/usr/bin/env node
+
 const readline = require("readline");
+const yargs = require('yargs/yargs');
+const { hideBin } = require('yargs/helpers')
 
 const calendarEvents = [];
+const argv = yargs(hideBin(process.argv)).argv;
 
+/**
+ * create a timestamp string from a date and a time 
+ * by default date and time are the current date and time.
+ * 
+ * @param {Date} date the date to format
+ * @param {Date} time the time to format
+ * @returns {string} the formatted timestamp
+ */
 const timestamp = (date = new Date(), time = new Date()) => {
   const year = date.getFullYear();
   const month = (date.getMonth() + 1).toString().padStart(2, "0");
@@ -13,6 +26,16 @@ const timestamp = (date = new Date(), time = new Date()) => {
   return `${year}${month}${day}T${hour}${minute}${second}Z`;
 };
 
+/**
+ * creates an event in ics format
+ * 
+ * @param {string} event_type the event type m,p,mp, etc..
+ * @param {string} title a description of the event
+ * @param {Date} date when the event will take place
+ * @param {Date} start when the event will start
+ * @param {Date} end when the event will ends
+ * @returns {Array<string>} an array of strings that represent the event in ics format
+ */
 const event = (event_type, title, date, start, end) => {
   const eventTimestamp = timestamp();
 
@@ -35,6 +58,12 @@ const event = (event_type, title, date, start, end) => {
   ];
 };
 
+/**
+ * a full ics calendar of events
+ * 
+ * @param {Array<string>} events list of events in ics format
+ * @returns {Array<string>} a string that represents a calendar in ics format
+ */
 const ics = (events) => {
   return [
     "BEGIN:VCALENDAR",
@@ -54,10 +83,10 @@ const rl = readline.createInterface({
 });
 
 rl.on("line", (line) => {
-  const shifts = line.split(",");
+  const shifts = line.split(",").map(x=>x.toLowerCase());
 
-  const year = 2025;
-  const month = 3;
+  const year = argv.year || 2025;
+  const month = argv.month || 1;
   const startDate = new Date(`${year}-${month}-01`);
 
   let index = 0;
@@ -84,6 +113,18 @@ rl.on("line", (line) => {
         endTime.setHours(19, 0, 0, 0);
     
         calendarEvents.push(...event(s, "Turno tarde", shiftDate, startTime, endTime));
+    }
+
+    // morning and afternoon shift
+    if (s === "mp") {
+        const shiftDate = new Date(startDate);
+        shiftDate.setDate(shiftDate.getDate() + index);
+        const startTime = new Date(shiftDate);
+        startTime.setHours(7, 0, 0, 0);
+        const endTime = new Date(shiftDate);
+        endTime.setHours(19, 0, 0, 0);
+        
+        calendarEvents.push(...event(s, "Turno mañana+tarde", shiftDate, startTime, endTime));        
     }
     
     // increment day counter
